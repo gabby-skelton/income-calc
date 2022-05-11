497d5b3ba7ced8c85f67262addf0b1a970ca36d4
Gabrielle Skelton <gabrielleskeltonn@gmail.com>
2022-05-11 13:08:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 function calculate() {
   var earnings = document.getElementById("earnings").value;
   earnings = parseInt(earnings, 10);
-  
-  var expense = document.getElementsByClassName("expense"); 
+
+  var expense = document.getElementsByClassName("expense");
   var expenseLength = expense.length;
   var expenseTotal = 0;
   for(i = 0; i < expenseLength; i++) {
-    expenseTotal += parseInt(expense[i].value);   
+    console.log(i);
+    var newExpValue = expense[i].value;
+
+    if(newExpValue == "") {
+      console.log("no value");
+      newExpValue = 0;
+    }
+    console.log(newExpValue);
+    expenseTotal += parseInt(newExpValue);
   }
-  
+
   var income = earnings - expenseTotal;
-  
+
   var par = document.getElementById("income");
-  par.innerHTML = "Your monthly income is $" + income;
+  par.innerHTML = "Your monthly income is $" + income + ".";
 }
